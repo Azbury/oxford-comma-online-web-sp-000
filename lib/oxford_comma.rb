2d080e4074a55ff61f6899096e408c2f4cfb7e8f
Austin Asbury <acasbury25@aol.com>
2019-08-17 23:31:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
   counter = 0
   string = ""
-  if array.size == 1
-    return array[0]
-  end
+  return array[0] if array.size == 1
   while counter < array.size
     if counter == array.size - 1
       string << " and #{array[counter]}"
